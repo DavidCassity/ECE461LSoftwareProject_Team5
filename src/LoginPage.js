622e0d84c83595ef4ddf30ce59e8380a648306a4
DavidCassity <davitrton123@gmail.com>
2024-02-20 19:49:53 -0600

    centered loginpage on screen; some readability updates

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -20,11 +20,11 @@ const LoginPage = () => {
     <div className="LoginPage">
       <h1>Login Page</h1>
       <div>
-        <label>Username:</label>
+        <label>Username: </label>
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
       </div>
       <div>
-        <label>Password:</label>
+        <label>Password: </label>
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
       <button onClick={handleLogin}>Login</button>
