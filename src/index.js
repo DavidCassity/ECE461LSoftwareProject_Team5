03fe1ba00e64da42cf6433d1d766b3a9351f746d
MaxSanchez99 <maxsanchez99.ms@gmail.com>
2024-02-21 13:03:56 -0600

    Adding a title

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import LoginPage from './LoginPage';
 import reportWebVitals from './reportWebVitals';
 
+import Title from './Title';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Title />
     <LoginPage />
   </React.StrictMode>
 );
