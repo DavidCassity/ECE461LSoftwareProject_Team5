7cdf3c6976b3e93cb3db2b037d021bfc2173c6b0
DavidCassity <davitrton123@gmail.com>
2024-02-21 13:45:36 -0600

    don't use text, it doesn't work properly

---
+++ src/Components.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export class textBox extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            label: props.label,
+            type: props.type
+        };
+    }
+
+    render(){
+        const {label, type} = this.state;
+        text = '';
+
+        return(
+            <div>
+                    <div>
+                        <label>{label}: </label>
+                    </div>
+            </div>
+        );
+
+    }
+}
