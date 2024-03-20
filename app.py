43e702fffcfc942164ef1dce33a45a1f8e2d7b2c
DavidCassity <davitrton123@gmail.com>
2024-03-20 13:52:59 -0500

    Merge branch 'main' into project-joining-and-popups

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ with open("login.txt", "r") as file:
     uri = file.read()
 
 # MongoDB connection
-uri = ""
-with open("login.txt", "r") as file:
-    uri = file.read()
-
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client.Users
 users = db.users
@@ -112,5 +108,4 @@ def serve_static():
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False, port=os.environ.get("PORT", 80))
     app.run(host="0.0.0.0", debug=False, port=os.environ.get("PORT", 80))
