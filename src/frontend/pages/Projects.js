133baf0548286ecce14da0cdbd2e6f72d07024df
MaxSanchez99 <maxsanchez99.ms@gmail.com>
2024-04-03 01:19:16 -0500

    User projects are displaying properly now

--- src/frontend/pages/Projects.js
+++ src/frontend/pages/Projects.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import { Grid, Button } from '@mui/material';
+import { Button } from '@mui/material';
 import ProjectCard from '../components/ProjectCard';
 import ProjectCreationPopup from '../components/ProjectCreationPopup';
 import ProjectJoinPopup from '../components/ProjectJoinPopup';
-import '../components/Banner.css';
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
@@ -14,31 +13,33 @@ const Projects = () => {
   const [userID, setUserID] = useState(''); // Add this line
   const navigate = useNavigate();
 
+
+  // Function to fetch projects from the server
+  const fetchProjects = async () => {
+    try {
+      const response = await fetch('/projects', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log("API response: ", data);
+      setAuthenticated(data.authenticated);
+      setUserID(data.userID);
+      setProjects(data.projects);
+      console.log(data.authenticated);
+      if (!data.authenticated) {
+        navigate('/login');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   
   // Call this function when first visiting the page
   useEffect(() => {
-    const handleChecking = async() => {
-      try {
-        const response = await fetch('/projects', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json', // Set Content-Type to JSON
-          },
-        });
-        const data = await response.json();
-        console.log("API response: ", data);
-        setAuthenticated(data.authenticated);
-        setUserID(data.userID);
-        console.log(data.authenticated);
-        if(!data.authenticated) {
-          navigate('/login');
-        }
-      }
-      catch (error) {
-        console.error('Error:', error);
-      }
-    };
-    handleChecking();
+    fetchProjects();
   }, []); // Pass an empty array to only run once
 
   const handleAddProject = () => {
@@ -49,30 +50,35 @@ const Projects = () => {
     setShowJoinPopup(true);
   };
 
+  const handleSuccess = async () => {
+    await fetchProjects();
+    setShowCreationPopup(false);
+    setShowJoinPopup(false);
+  }
+
   return (
     <div>
-      <div className="global-capacity-banner">
-        <h2 className="banner-title">Global Capacity</h2>
-        <div className="meter-container">
-          <div className="meter" style={{ width: `${(0 / 1) * 100}%`, backgroundColor: 'green' }}>
-            <p className="checked-out-text">{0}/{1}</p>
-          </div>
-        </div>
-      </div>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Button fullWidth variant="contained" onClick={handleAddProject} className="add-project-button">
-            Add Project
-          </Button>
-        </Grid>
-        <Grid item xs={6}>
-          <Button fullWidth variant="contained" onClick={handleJoinProject} className="join-project-button">
-            Join Project
-          </Button>
-        </Grid>
-      </Grid>
-      {showCreationPopup && <ProjectCreationPopup usernameID={userID} onClose={() => setShowCreationPopup(false)} />}
-      {showJoinPopup && <ProjectJoinPopup onClose={() => setShowJoinPopup(false)} />}
+      {/* Iterate over each project in the projects array */}
+      {projects.map(project => (
+        <ProjectCard key={project.projectID} 
+            projectID={project.projectID} 
+            ownerID={project.ownerID} 
+            members={project.members}
+            checkOut={project.checkOut}
+            availability={project.availability}
+            capacity={project.capacity}
+            userID={userID}
+            />
+      ))}
+      <Button variant="contained" onClick={handleAddProject} style={{ position: 'center', marginTop: '20px' }}>
+        Add Project
+      </Button>
+      <Button variant="contained" onClick={handleJoinProject} style={{ position: 'center', marginTop: '20px'}}>
+        Join Project
+      </Button>
+
+      {showCreationPopup && <ProjectCreationPopup usernameID={userID} onClose={handleSuccess} />}
+      {showJoinPopup && <ProjectJoinPopup onClose={handleSuccess} />}
     </div> 
   );
 };
