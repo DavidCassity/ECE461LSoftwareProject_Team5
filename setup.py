6ad8c2aebf64b1cb9041c69a493c1fd2907f6138
MaxSanchez99 <maxsanchez99.ms@gmail.com>
2024-04-07 00:36:41 -0500

    created files for heroku

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'Flask==3.0.2',
+        'Flask_Bcrypt==1.0.1',
+        'Flask_Cors==4.0.0',
+        'Flask_Login==0.6.3',
+        'pymongo==4.6.2',
+    ],
+)
